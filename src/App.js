8e4611d14559c5068f62a677953d6e5e667fdc4b
yessyprmtsr <yessypermatasari8@gmail.com>
2021-07-25 19:33:36 +0700

    add navBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import Hero from './components/Hero';
 import Navbar from './components/Navbar'
+import GlobalStyle from './globalStyles';
 
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <GlobalStyle></GlobalStyle>
       <Navbar/>
-    </div>
+      <Hero/>
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,97 @@
-import React from 'react'
+import React from "react";
+import styled, { css } from "styled-components/macro";
+import { Link } from "react-router-dom";
+import { menuData } from "../data/MenuData";
+import { Button } from "./Button";
+import Bars from '../images/bars.svg';
+
+
+const Nav = styled.nav`
+  background: red;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 2rem;
+  z-index: 100;
+  position: fixed;
+  width: 100%;
+  height: 68px;
+`;
+
+const NavLink = css`
+  color: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+  text-decoration: none;
+`;
+
+const Logo = styled(Link)`
+  ${NavLink}
+  font-style: italic;
+  font-weight: bold;
+`;
+
+const MenuBars = styled.i`
+  display: none;
+  @media screen and (max-width: 760px) {
+    display: block;
+    background-image:url(${Bars});
+    background-size:contain;
+    height:40px;
+    width: 40px;
+    position:absolute;
+    cursor:pointer;
+    top:0;
+    right:0;
+    transform: translate(-50%, 15%);
+  }
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: -48px;
+
+  @media screen  and (max-width: 760px) {
+      display: none;
+  }
+`;
+
+const NavMenuLinks = styled(Link)`
+  ${NavLink}
+`;
+
+const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
+
+  @media screen and (max-width: 760px) {
+    display: none;
+  }
+`;
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
-    )
-}
+  return (
+    <Nav>
+      <Logo to="/">ICESTATE</Logo>
+      <MenuBars />
+      <NavMenu>
+        {menuData.map((item, index) => (
+          <NavMenuLinks to={item.link} key={index}>
+            {item.title}
+          </NavMenuLinks>
+        ))}
+      </NavMenu>
+      <NavBtn>
+        <Button to="/contact" primary="true">
+          Contact Us
+        </Button>
+      </NavBtn>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
